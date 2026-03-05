4e98286bde7b18284c60fadc91cc41929d0dc5ab
Brendan <brendanw7@gmail.com>
2026-03-04 21:51:29 -0700

    Add vue integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import vue from '@astrojs/vue';
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://brendanwolfe.me",
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [vue()]
 });
